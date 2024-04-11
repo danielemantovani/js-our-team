4971e953ef8d80081983aa0f66fc32ea21cfa54c
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-11 16:49:39 +0200

    feat: stampa in console le info di ogni membro del team

--- js/script.js
+++ js/script.js
@@ -1,34 +1,70 @@
+// CREO ARRAY DI OGGETTI 
+
 const memberInfo = [
     {
         name: "Wayne Barnett",
-        positio: "Founder & CEO",
+        position: "Founder & CEO",
         photo: "wayne-barnett-founder-ceo.jpg"
     },
     {
         name: "Angela Caroll",
-        positio: "Chief Editor",
+        position: "Chief Editor",
         photo: "angela-caroll-chief-editor.jpg"
     },
     {
         name: "Walter Gordon",
-        positio: "Office Manager",
+        position: "Office Manager",
         photo: "walter-gordon-office-manager.jpg"
     },
     {
         name: "Angela Lopez",
-        positio: "Social Media Manager",
+        position: "Social Media Manager",
         photo: "angela-lopez-social-media-manager.jpg"
     },
     {
         name: "Scott Estrada",
-        positio: "Developer",
+        position: "Developer",
         photo: "scott-estrada-developer.jpg"
     },
     {
         name: "Barbara Ramos",
-        positio: "Graphic Designer",
+        position: "Graphic Designer",
         photo: "barbara-ramos-graphic-designer.jpg"
     }
 ]
 
-console.log(memberInfo);
+// console.log(memberInfo);
+
+
+// STAMPO IN CONSOLE CON CICLO FOR
+
+for (let i= 0; i < memberInfo.length; i++){
+    let currMember = memberInfo [i]; //object
+
+//   console.log(currMember);
+//   console.log ("***********************")
+
+// stampo in console con dot notation e braket notation i valori dell'oggetto
+
+//   console.log(currMember.name);
+//   console.log(currMember ["position"]);
+
+
+// STAMPO TUTTE LE INFORMAZIONI DI OGNI MEMBRO DEL TEAM IN CONSOLE UTILIZZANDO IL CICLO FOR IN
+
+    for (let key in currMember){
+    console.log (currMember[key]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
