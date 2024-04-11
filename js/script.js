27db4841b9686d2cb0239b0138f45fbf4084b52f
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-11 18:55:19 +0200

    feat: stampa in pagina

--- js/script.js
+++ js/script.js
@@ -4,32 +4,32 @@ const memberInfo = [
     {
         name: "Wayne Barnett",
         position: "Founder & CEO",
-        photo: "wayne-barnett-founder-ceo.jpg"
+        photo: "./img/wayne-barnett-founder-ceo.jpg"
     },
     {
         name: "Angela Caroll",
         position: "Chief Editor",
-        photo: "angela-caroll-chief-editor.jpg"
+        photo: "./img/angela-caroll-chief-editor.jpg"
     },
     {
         name: "Walter Gordon",
         position: "Office Manager",
-        photo: "walter-gordon-office-manager.jpg"
+        photo: "./img/walter-gordon-office-manager.jpg"
     },
     {
         name: "Angela Lopez",
         position: "Social Media Manager",
-        photo: "angela-lopez-social-media-manager.jpg"
+        photo: "./img/angela-lopez-social-media-manager.jpg"
     },
     {
         name: "Scott Estrada",
         position: "Developer",
-        photo: "scott-estrada-developer.jpg"
+        photo: "./img/scott-estrada-developer.jpg"
     },
     {
         name: "Barbara Ramos",
         position: "Graphic Designer",
-        photo: "barbara-ramos-graphic-designer.jpg"
+        photo: "./img/barbara-ramos-graphic-designer.jpg"
     }
 ]
 
@@ -58,6 +58,20 @@ for (let i= 0; i < memberInfo.length; i++){
 }
 
 
+//STAMPO IN PAGINA
+
+memberInfo.forEach ((currMember) =>{
+    // console.log (currMember);
+    const member = document.getElementById ("member-team");
+    // console.log(stampMember);
+
+    const currMemberStamp = `
+        <h2>${currMember.name}</h3>
+        <p>${currMember.position}</p>
+        <img src="${currMember.photo}">
+    `
+    member.innerHTML += currMemberStamp;
+})
 
 
 
